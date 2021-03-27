20762058dae26438443b448273ce93d8245fa78b
User <User@DESKTOP-RB452N0>
2021-03-27 09:25:55 +0530

    Priority=2

---
+++ src/main/java/Bootcamp/testng/SAL_08.java
@@ -0,0 +1,30 @@
+package Bootcamp.testng;
+
+
+
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import org.testng.annotations.Test;
+
+
+public class SAL_08 extends Base_class {
+	@Test(priority=2)
+public void basic() throws InterruptedException 
+{
+		
+	Thread.sleep(9000);
+    driver.findElementByXPath("//div[@class='slds-icon-waffle']").click();
+	Thread.sleep(8000);
+	driver.findElementByXPath("//button[@class='slds-button']").click();
+	Thread.sleep(1000);
+	WebElement path = driver.findElementByXPath("//p[text()='Sales']");
+	JavascriptExecutor exe= (JavascriptExecutor) driver;
+	exe.executeScript("arguments[0].click()", path);
+	Thread.sleep(3000);
+	
+	}
+	
+}
+
